ba4395d911ccdbb69bd3d1eadcc2451caf2b1556
Mark Phillips <mgphillips1@gmail.com>
2020-08-08 15:04:49 -0400

    Initial commit, x86 build added

---
+++ RTS-Tests/PS/browserTests.ps1
@@ -0,0 +1,29 @@
+﻿
+param($appName, $serverEnv, $testCategory)
+
+$appEnv = $appName + '_' + $serverEnv
+$header = '"TestEnvironment": "' + $appName + '_' + $serverEnv + '"'
+
+#Start in PS folder
+
+Set-Location ($PSScriptRoot)
+
+#Move up to project folder
+
+sl ..
+
+ 
+
+# update test environment in nunit configuration file
+
+#(gc Configuration\UserDefinedConfigurations.json) -replace '\"TestEnvironment\": \"(\w+).*\"', $header | Out-File -encoding ASCII Configuration\UserDefinedConfigurations.json
+
+ 
+
+#Move up to solution folder
+
+sl ..
+
+#execute tests
+
+nunit3-console.exe "RTS-Tests\bin\x86\Release\RTS-Tests.dll"
